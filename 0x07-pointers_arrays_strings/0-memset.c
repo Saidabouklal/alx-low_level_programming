166894fa7413e543e12cdfd540f93559955efc7d
Saidabouklal <saidabouklal3@gmail.com>
2023-04-03 16:07:38 -0700

    0-memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * *_memset - Entry point
+ * @s: pointer to put the constant
+ * @b: constant
+ * @n: bytes
+ * Return: Always 0 (Success)
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+s[i] = b;
+return (s);
+}
