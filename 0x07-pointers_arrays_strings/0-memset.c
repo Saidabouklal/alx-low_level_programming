8a45105568f13a96a1563a1d5b288cbbe8da9a2e
Saidabouklal <saidabouklal3@gmail.com>
2023-04-04 14:42:16 -0700

    0x07

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
- * *_memset - Entry point
- * @s: pointer to put the constant
- * @b: constant
- * @n: bytes
- * Return: Always 0 (Success)
+ * _memset - ironnou
+ * @s: pointer to char params
+ * @b: data to change
+ * @n: index
+ * Return: *s
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i;
+	unsigned int i;
 
-for (i = 0; i < n; i++)
-s[i] = b;
-return (s);
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
 }
