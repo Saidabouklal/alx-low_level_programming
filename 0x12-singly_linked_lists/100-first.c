66d6aca7feee433d9a31531bd55142286dcd3d8b
Saidabouklal <saidabouklal3@gmail.com>
2023-04-28 17:33:32 -0700

    b

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * before_main - before main fnct
+ *
+ * Return: void
+ */
+
+
+void __attribute__ ((constructor)) before_main()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
